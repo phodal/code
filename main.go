eae9672217375e920f6a8acc02e4a9c5aba573cf
Phodal Huang <h@phodal.com>
2019-11-29 08:59:20 +0800

    refactor: extract method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	. "./model"
 	. "./parser/code"
 	. "./parser/define"
 	codetemplate "./parser/template"
@@ -20,30 +21,27 @@ func main() {
 	defineApp := NewDefineApp(startTemplateSymbol, endTemplateSymbol)
 	info := defineApp.Start("examples/mu.define")
 
-	transform := &Transform{}
+	transformCode(codeModel, info, startTemplateSymbol, endTemplateSymbol)
+}
 
+func transformCode(codeModel CodeModel, info DefineInformation, startTemplateSymbol string, endTemplateSymbol string) {
+	transform := &Transform{}
 	var packageInfo string
 	var imports []string
 	var code = ""
 	var result = ""
-
 	for _, call := range codeModel.FunctionCalls {
-		code = code  + "\n" + transform.ToCode(call, info.ModuleDeclarations)
+		code = code + "\n" + transform.ToCode(call, info.ModuleDeclarations)
 		imports = transform.BuildImport(call, info.ModuleDeclarations)
 	}
-
 	templates := info.DefineTemplates
 	template := codetemplate.New(templates["code"], startTemplateSymbol, endTemplateSymbol)
 	result = template.ExecuteString(map[string]interface{}{
 		"code": code,
 	})
-
 	packageInfo = transform.BuildPackage("main")
-
 	codeWithImport := packageInfo + strings.Join(imports, "") + result
-
 	_ = ioutil.WriteFile("test/main/main.go", []byte(codeWithImport), 0644)
-
 	cmd := exec.Command("go", "run", "test/main/main.go")
 	stdout, err := cmd.StdoutPipe()
 	_ = cmd.Start()
